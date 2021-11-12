9532b9d8f3b1738c7982d19aa92581d89f47ecaf
allan galdino <galdinoallancodes@gmail.com>
2021-11-12 19:03:48 -0300

    loggin request information

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,7 +1,32 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
 void findAll() async {
+  final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
   final Response response =
-      await get(Uri.parse('http://192.168.15.48:8080/transactions'));
-  print(response.body);
+      await client.get(Uri.parse('http://192.168.15.48:8080/transactions'));
+}
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({@required RequestData data}) async {
+    print('Request url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({@required ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
 }
