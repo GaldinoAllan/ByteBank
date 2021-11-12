d83ef67d13d2c8ec0317a971426fe83cf8557626
allan galdino <galdinoallancodes@gmail.com>
2021-11-12 18:46:58 -0300

    wip: implementando webapi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:bytebank/database/app_db.dart';
+import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(BytebankApp());
+  findAll();
 }
 
 class BytebankApp extends StatelessWidget {
