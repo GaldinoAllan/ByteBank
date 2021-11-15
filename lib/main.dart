f0fece5e18dc6343c5543cc1979c4791e9b87b7e
allan galdino <galdinoallancodes@gmail.com>
2021-11-15 12:40:34 -0300

    carregando lista de transferencias na tela

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(BytebankApp());
-  findAll().then((transactions) => print("new transactions $transactions"));
 }
 
 class BytebankApp extends StatelessWidget {
