559ee2ec965173c14f9324161afadfff8ae628ee
allan galdino <galdinoallancodes@gmail.com>
2021-11-17 17:33:53 -0300

    Integrando dialog de autenticação

--- lib/components/transaction_auth_dialog.dart
+++ lib/components/transaction_auth_dialog.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
-class TransactionAuthDialog extends StatelessWidget {
+class TransactionAuthDialog extends StatefulWidget {
+  final Function(String password) onConfirm;
+
+  TransactionAuthDialog({@required this.onConfirm});
+
+  @override
+  _TransactionAuthDialogState createState() => _TransactionAuthDialogState();
+}
+
+class _TransactionAuthDialogState extends State<TransactionAuthDialog> {
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Authenticate'),
       content: TextField(
+        controller: _passwordController,
         obscureText: true,
         maxLength: 4,
         decoration: InputDecoration(
@@ -15,19 +27,18 @@ class TransactionAuthDialog extends StatelessWidget {
         keyboardType: TextInputType.number,
         style: TextStyle(
           fontSize: 64,
-          letterSpacing: 32,
+          letterSpacing: 24,
         ),
       ),
       actions: <Widget>[
         TextButton(
-          onPressed: () {
-            print('Cancel');
-          },
+          onPressed: () => Navigator.pop(context),
           child: Text('Cancel'),
         ),
         TextButton(
           onPressed: () {
-            print('confirm');
+            widget.onConfirm(_passwordController.text);
+            Navigator.pop(context);
           },
           child: Text('Confirm'),
         )
