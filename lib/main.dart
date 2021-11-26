31aa77abe4e4e35de9ad3a89cbdd837d4c049899
allan galdino <galdinoallancodes@gmail.com>
2021-11-26 18:14:06 -0300

    gerenciador de estado, interface com o usuario e container

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:bytebank/components/theme.dart';
+import 'package:bytebank/screens/counter.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +12,8 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.green[900],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary,
-        ),
-      ),
-      home: Dashboard(),
+      theme: bytebankTheme,
+      home: CounterContainer(),
     );
   }
 }
